aabafbd8204336ac92305e97be4a6bb0f1255a3e
Marc Trius <derpayatz@gmail.com>
2020-10-01 11:01:12 -0500

    refactor App to use state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import List from './List'
+import STORE from './STORE';
+
+import List from './List';
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+  state = STORE;
 
   render() {
-    const { store } = this.props
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className='App-list'>
-          {store.lists.map(list => (
+          {this.state.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => this.state.allCards[id])}
             />
           ))}
         </div>
